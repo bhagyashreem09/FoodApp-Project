cb9063eb27950d1954bbf4e0bf503b514e30cc50
Bhagyashree <bhagyashreemarde09@gmail.com>
2022-03-12 02:13:22 +0530

    Cart logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,8 +10,28 @@ const defaultCartState = {
 
 function cartReducer(state, action) {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.amount;
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(
+            item => (item.id === action.item.id)
+            );  //will find index of an item in an array
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+        
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items : updatedItems,
             totalAmount : updatedTotalAmount
@@ -24,11 +44,11 @@ function CartProvider(props) {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
     function addItemToCartHandler (item) {
-        dispatchCartAction({type : 'ADD', item: item})
+        dispatchCartAction({type : 'ADD', item : item})
     }
 
     function removeItemFromCartHandler (id) {
-
+        dispatchCartAction({type : 'REMOVE', id : id})
     }
 
     const cartContext = {
